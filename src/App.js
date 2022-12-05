9a2baccf33f01067b448ab0fe867e6c985104d54
Zammon_V <87796940+Zammon@users.noreply.github.com>
2022-12-05 09:44:24 +0700

    Update! Dropsideway-Admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +20,24 @@ function App() {
         </a>
       </header>
     </div>
+=======
+    <>
+      <Slidebar admin={admins[0]} />
+      <Navbar admin={admins[0]}/>
+      <Routes>
+        {/* <Route path='*' element={<Error404 />} /> */}
+        
+        <Route path="/" element={<Home />} />
+        <Route path='/managepost' element={<ManagePost />} />
+        <Route path='/manageuser' element={<ManageUser />} />
+        <Route path='/managereport' element={<ManageReport />} />
+        <Route path='/profile' element={<AdminProfile />} />
+        <Route path='/manageadmin' element={<AdminManage />} />
+        <Route path='/login' element={<Login />}/>
+        <Route path='*' element={<Error404 />}/>
+      </Routes>
+    </>
+>>>>>>> Stashed changes
   );
 }
 
